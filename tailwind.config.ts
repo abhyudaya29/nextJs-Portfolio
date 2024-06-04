b2f5e57f5b230d570a616c898548fe371e8083d5
abhyudaya29 <abhyudayadev123@gmail.com>
2024-06-05 02:04:47 +0530

    tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,9 @@
 import type { Config } from "tailwindcss"
-
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 const config = {
   darkMode: ["class"],
   content: [
@@ -75,6 +79,17 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
+  addVariablesForColors
 } satisfies Config
-
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
 export default config
+
